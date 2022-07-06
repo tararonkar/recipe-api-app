61c1df628207188784346ded399f67f418c04e87
Onkar Tarar <tararonkar@protonmail.com>
2022-07-06 23:30:37 +0530

    Added postgresql config in docker-compose file, install postgresql adapter

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x,y):
+    # Add x and y and return result.
+    return x+y
+
+def subtract(x,y):
+    """ Subtract x from y and resturn result """
+    return y-x
